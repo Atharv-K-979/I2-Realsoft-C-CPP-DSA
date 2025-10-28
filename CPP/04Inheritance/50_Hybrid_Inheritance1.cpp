500a19072e66035fd8bb4fde4f96179b524bf016
Atharv Kulakrni <atharvkulkarni979@gmail.com>
2025-10-28 07:49:20 +0530

    Added

---
+++ CPP/04Inheritance/50_Hybrid_Inheritance1.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <limits>
+#include <cstring>
+#include <cstdlib>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+class Number
+{
+protected:
+    int no;
+
+public:
+    Number() { no = 0; }
+    ~Number() {}
+    void setData()
+    {
+        cout << "\nNo: ";
+        cin >> no;
+    }
+    void display()
+    {
+        cout << "\nNo: " << no;
+    }
+    int getNo() { return no; }
+};
+class Name : public Number
+{
+protected:
+    string nm;
+
+public:
+    Name() : Number() {}
+    ~Name() {}
+    void setData()
+    {
+        Number::setData();
+        cout << "\nName: ";
+        cin >> nm;
+    }
+    void display()
+    {
+        Number::display();
+        cout << "\nName: " << nm << endl;
+    }
+    string getName() { return nm; }
+};
+class Sports
+{
+protected:
+    int smrk;
+
+public:
+    Sports() { smrk = 0; }
+    ~Sports() {}
+    void setData()
+    {
+        cout << "\nSports Marks: ";
+        cin >> smrk;
+    }
+    void display()
+    {
+        cout << "\nSports marks are: " << smrk;
+    }
+    int getMrk() { return smrk; }
+};
+class Result : public Name, Sports
+{
+protected:
+    int tmrk;
+
+public:
+    Result() : Name(), Sports() { tmrk = 0; }
+    ~Result() {}
+    void setData()
+    {
+        Name::setData();
+        Sports::setData();
+        cout << "\nTotal Marks: ";
+        cin >> tmrk;
+    }
+    void display()
+    {
+        Name::display();
+        Sports::display();
+        cout << "\nTotal Marks: " << tmrk;
+    }
+    int getTotalMrk() { return tmrk; }
+};
+int main()
+{
+    Result r;
+    r.setData();
+    r.display();
+
+    Number a;
+    a.setData();
+    a.display();
+
+    Number b;
+    b.setData();
+    b.display();
+    return 0;
+}
